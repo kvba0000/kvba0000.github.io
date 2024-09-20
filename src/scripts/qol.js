64c1d5c7ade49cdd90c3281c450125e1e1ca0885
kvba <pojg77g1@kbb.anonaddy.me>
2024-09-20 17:38:15 +0200

    new cursor on site

--- src/scripts/qol.js
+++ src/scripts/qol.js
@@ -1,3 +1,5 @@
+const SELECTABLE_TAGS = ["A", "BUTTON", "INPUT", "SELECT", "TEXTAREA", "DETAILS"]
+
 // Prevent default drag and drop
 document.ondragstart = (e) => e.preventDefault();
 
@@ -12,3 +14,48 @@ const calculateAge = () => {
     document.getElementById("age").textContent = age;
 }
 calculateAge();
+
+// Cursor Effect
+const prepareCursor = () => {
+    const cursorStyle = document.createElement("style");
+    cursorStyle.innerHTML = `* { cursor: none !important; }`;
+    document.head.appendChild(cursorStyle);
+
+    const cursor = document.getElementById("cursor");
+    const cursorSize = cursor.getBoundingClientRect()
+    const gridSize = {
+        x: cursorSize.width,
+        y: cursorSize.height
+    }
+    const cursorPosition = {
+        x: 0,
+        y: 0
+    }
+
+    const updateCursor = () => {
+        const x = Math.floor((cursorPosition.x + scrollX) / gridSize.x) * gridSize.x;
+        const y = Math.floor((cursorPosition.y + scrollY) / gridSize.y) * gridSize.y;
+        cursor.style.left = x + "px";
+        cursor.style.top = y + "px";
+
+        const hoveringTags = document.elementsFromPoint(cursorPosition.x, cursorPosition.y).map(el => el.tagName)
+
+        if(
+            SELECTABLE_TAGS.some(tag => hoveringTags.includes(tag))
+        ) cursor.classList.add("hover");
+        else cursor.classList.remove("hover");
+    }
+
+    document.addEventListener("mousemove", (e) => {
+        cursorPosition.x = e.clientX;
+        cursorPosition.y = e.clientY;
+        updateCursor();
+    })
+    document.addEventListener("scroll", updateCursor)
+    window.addEventListener("resize", updateCursor)
+
+    window.addEventListener("mouseover", () => cursor.classList.remove("out"))
+    window.addEventListener("mouseout", () => cursor.classList.add("out"))
+        
+}
+prepareCursor()
